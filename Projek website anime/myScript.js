cec130005a7acfd60b533a14cf9f72c65c4f11d7
Danannme <noragamiaragotoyato92@gmail.com>
2023-03-23 08:17:35 +0700

    menghapus Project website anime, login page, dan tribute page

--- Projek website anime/myScript.js
+++
@@ -1,9 +0,0 @@
-alert("Selamat Datang")
-var nama = prompt("Masukkan nama anda:");
-var namalain = confirm("apa benar nama anda " + nama +" ?");
-if(namalain === true) {
-    alert("Hai " + nama);
-}   else{
-    var namaganti = prompt("Ganti nama anda:");
-    alert("Nama berhasil di ganti");
-}  
