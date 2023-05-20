467d7f30ffaa27c1817a089cc93e1eaa7f66cb76
Danannme <124498667+Danannme@users.noreply.github.com>
2023-05-20 19:51:33 +0700

    Delete Project Website Profile Kelas X-G directory

--- Project Website Profile Kelas X-G/js/script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('.menu .menu-icon input'); 
-const nav = document.querySelector('.menu ul');
-
-menuIcon.addEventListener('click', function() {
-    nav.classList.toggle('slide');
-});
